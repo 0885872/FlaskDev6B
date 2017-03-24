ef02b0d9aaba94b5aa5d5dd045f66f74b1f9dd95
Tim <0885872@hr.nl>
2017-03-24 01:26:43 +0100

    first real test commit
    first real test commit

--- FlaskWebProject3/Controller.py
+++ FlaskWebProject3/Controller.py
@@ -4,15 +4,15 @@ import app
 #Class isn't doing anything anymore
 
 class Controller(object):
-    def home():
-        return render_template('index.html')
-    def w1():
-        return render_template('w1.html')
-    def ttev1(a):
-        return render_template('ttev1.html', a)
-    def ttev2():
-        return render_template('ttev2.html')
-    def w2():
-        return render_template('w2.html')
+    #def home():
+    #    return render_template('index.html')
+    #def w1():
+    #    return render_template('w1.html')
+    #def ttev1(a):
+    #    return render_template('ttev1.html', a)
+    #def ttev2():
+    #    return render_template('ttev2.html')
+    #def w2():
+    #    return render_template('w2.html')
 
 
